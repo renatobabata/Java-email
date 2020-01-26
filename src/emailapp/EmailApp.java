6e427def1bc8bd67ee5197d3e3938ef7a42799c1
Renato Babata <babatarenato@gmail.com>
2020-01-26 16:22:34 -0300

    Java projects

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,12 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Email email = new Email("Renato","Babata");
+		System.out.println(email.showInfo());
+	}
+
+}
